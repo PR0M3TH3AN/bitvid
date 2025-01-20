035ce99bc26ee79d217d15c2e93e8d56afa7064d
Keep Creating Online <adambrettmalin@protonmail.com>
2025-01-19 19:42:39 -0500

    update

--- refactoring/js/config.js
+++
@@ -1,4 +0,0 @@
-// js/config.js
-
-export const isDevMode = true; // Set to false for production
-export const isWhitelistEnabled = true; // Set to false to allow all non-blacklisted users
--- refactoring/js/utils/htmlUtils.js
+++
@@ -1,9 +0,0 @@
-// js/utils/htmlUtils.js
-export function escapeHTML(unsafe) {
-  return unsafe
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;")
-    .replace(/'/g, "&#039;");
-}
