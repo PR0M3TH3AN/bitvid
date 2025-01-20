61338f3d2da94942b0556f8ab17a34b3b4eb605a
Keep Creating Online <adambrettmalin@protonmail.com>
2025-01-19 20:02:18 -0500

    update

--- src/js/lists.js
+++ src/js/lists.js
@@ -5,6 +5,7 @@ const npubs = [
   "npub15jnttpymeytm80hatjqcvhhqhzrhx6gxp8pq0wn93rhnu8s9h9dsha32lx",
   "npub1j37gc05qpqzyrmdc5vetsc9h5qtstas7tr25j0n9sdpqxghz6m4q2ej6n8",
   "npub1epvnvv3kskvpnmpqgnm2atevsmdferhp7dg2s0yc7uc0hdmqmgssx09tu2",
+  "npub1qny3tkh0acurzla8x3zy4nhrjz5zd8l9sy9jys09umwng00manysew95gx", // ODELL
 ];
 
 console.log("DEBUG: lists.js loaded, npubs:", npubs);
