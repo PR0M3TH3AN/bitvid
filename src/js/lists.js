58acbf3766437f331f9dbaa1aee8b85fad7db8de
Keep Creating Online <adambrettmalin@protonmail.com>
2025-01-23 19:30:01 -0500

    added NosToons to white list

--- src/js/lists.js
+++ src/js/lists.js
@@ -6,6 +6,7 @@ const npubs = [
   "npub1j37gc05qpqzyrmdc5vetsc9h5qtstas7tr25j0n9sdpqxghz6m4q2ej6n8",
   "npub1epvnvv3kskvpnmpqgnm2atevsmdferhp7dg2s0yc7uc0hdmqmgssx09tu2",
   "npub1qny3tkh0acurzla8x3zy4nhrjz5zd8l9sy9jys09umwng00manysew95gx", // ODELL
+  "npub1qxduthz4p8v5zsux524df569pt7lj0d36dyqadnta2val64dtnhsr50h64", // NosToons
 ];
 
 console.log("DEBUG: lists.js loaded, npubs:", npubs);
