6e6132d88249f1c6bd40f13177ef1bdea0aba7c9
Keep Creating Online <53631862+PR0M3TH3AN@users.noreply.github.com>
2025-02-01 23:02:51 -0500

    updated modals, forms, UI flow

--- src/js/disclaimer.js
+++ src/js/disclaimer.js
@@ -2,28 +2,35 @@ class DisclaimerModal {
   constructor() {
     this.modal = document.getElementById("disclaimerModal");
     this.acceptButton = document.getElementById("acceptDisclaimer");
+    // If user previously dismissed the disclaimer, we'll store "true" in localStorage:
     this.hasSeenDisclaimer = localStorage.getItem("hasSeenDisclaimer");
 
+    // Set up the click event for the "I Understand" button
     this.setupEventListeners();
   }
 
   setupEventListeners() {
-    const closeModal = () => {
-      this.modal.style.display = "none";
-      document.body.style.overflow = "unset";
-      localStorage.setItem("hasSeenDisclaimer", "true");
-    };
-
-    // Only keep the accept button event listener
-    this.acceptButton.addEventListener("click", closeModal);
+    if (this.acceptButton) {
+      this.acceptButton.addEventListener("click", () => {
+        // Hide the disclaimer by adding the "hidden" class
+        if (this.modal) {
+          this.modal.classList.add("hidden");
+        }
+        // Mark that the user has seen the disclaimer, so we don't show it again
+        localStorage.setItem("hasSeenDisclaimer", "true");
+      });
+    }
   }
 
   show() {
+    // Only show it if the user hasn't seen it before
     if (!this.hasSeenDisclaimer) {
-      this.modal.style.display = "flex";
-      document.body.style.overflow = "hidden";
+      if (this.modal) {
+        this.modal.classList.remove("hidden");
+      }
     }
   }
 }
 
+// Export an instance that you can import in your main script
 export const disclaimerModal = new DisclaimerModal();
