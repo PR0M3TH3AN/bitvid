cc627c1e65a188b16e8cf56c334f08a7bb882642
thePR0M3TH3AN <53631862+PR0M3TH3AN@users.noreply.github.com>
2025-09-27 20:47:55 -0400

    update

--- js/gridHealth.js
+++ js/gridHealth.js
@@ -165,7 +165,7 @@ function queueProbe(magnet, cacheKey, priority = 0) {
 
 const PROBE_CACHE_TTL_MS = 5 * 60 * 1000;
 const PROBE_TIMEOUT_MS = 8000;
-const PROBE_CONCURRENCY = 48;
+const PROBE_CONCURRENCY = 96;
 const PROBE_POLL_COUNT = 3;
 const PRIORITY_BASELINE = 1_000_000;
 
