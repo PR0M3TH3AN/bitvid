b3194f678f2f0b38519479f791a6d3387d297786
thePR0M3TH3AN <53631862+PR0M3TH3AN@users.noreply.github.com>
2025-10-10 21:31:17 -0400

    Enable CSS linting and replace legacy colors

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,18 +1,22 @@
+const VAR_PATTERN = /^var\(--.*\)$/;
+const CONTAINS_VAR_PATTERN = /var\(--.*\)/;
+const LINEAR_GRADIENT_PATTERN = /^linear-gradient\(/;
+
 module.exports = {
-  ignoreFiles: ['css/tailwind.generated.css', 'css/style.css'],
+  ignoreFiles: ['css/tailwind.generated.css', 'css/tailwind.min.css'],
   rules: {
     'at-rule-no-unknown': [true, { ignoreAtRules: ['tailwind', 'layer', 'apply'] }],
     'import-notation': 'string',
     'color-no-hex': true,
     'declaration-property-value-allowed-list': {
-      color: ['var\(--.*\)', 'inherit', 'currentColor'],
-      'background-color': ['var\(--.*\)', 'transparent', 'inherit'],
-      background: ['var\(--.*\)', 'none', 'transparent'],
-      'border-color': ['var\(--.*\)', 'transparent', 'inherit', 'currentColor'],
-      'outline-color': ['var\(--.*\)', 'transparent', 'inherit', 'currentColor'],
-      fill: ['var\(--.*\)', 'none', 'currentColor', 'inherit'],
-      stroke: ['var\(--.*\)', 'none', 'currentColor', 'inherit'],
-      'box-shadow': ['var\(--.*\)', 'none']
+      color: [VAR_PATTERN, 'inherit', 'currentColor'],
+      'background-color': [VAR_PATTERN, 'transparent', 'inherit'],
+      background: [VAR_PATTERN, CONTAINS_VAR_PATTERN, LINEAR_GRADIENT_PATTERN, 'none', 'transparent'],
+      'border-color': [VAR_PATTERN, CONTAINS_VAR_PATTERN, 'transparent', 'inherit', 'currentColor'],
+      'outline-color': [VAR_PATTERN, 'transparent', 'inherit', 'currentColor'],
+      fill: [VAR_PATTERN, 'none', 'currentColor', 'inherit'],
+      stroke: [VAR_PATTERN, 'none', 'currentColor', 'inherit'],
+      'box-shadow': [VAR_PATTERN, CONTAINS_VAR_PATTERN, 'none']
     }
   }
 };
