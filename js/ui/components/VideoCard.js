77052fed7c3f994d6ee48b820b66b2fe8196c742
thePR0M3TH3AN <53631862+PR0M3TH3AN@users.noreply.github.com>
2025-10-18 15:00:08 -0400

    Ensure Nostr profile avatars remain circular

--- js/ui/components/VideoCard.js
+++ js/ui/components/VideoCard.js
@@ -663,7 +663,15 @@ export class VideoCard {
         alt: "Placeholder"
       }
     });
-    avatar.classList.add("author-pic", "cursor-pointer");
+    avatar.classList.add(
+      "author-pic",
+      "cursor-pointer",
+      "block",
+      "h-full",
+      "w-full",
+      "rounded-full",
+      "object-cover"
+    );
     if (this.video.pubkey) {
       avatar.dataset.pubkey = this.video.pubkey;
     }
